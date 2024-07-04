abc299777a807c6f145669738f35c365c2a85367
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-07-04 12:28:54 +0600

    added base api system

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import languageReducer from "./features/languageSlice";
 import dataReducer from "./features/dataSlice";
+import { baseApi } from "./api/baseApi";
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]:baseApi.reducer,
     data: dataReducer,
     language: languageReducer,
   },
+  middleware:(getdefaultMiddlewares)=>getdefaultMiddlewares().concat(baseApi.middleware)
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
