f645e0446edf81cca803b669854d92d3b08607e2
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-07-06 16:21:43 +0600

    ok

--- app/components/RecentSellTicketPart.tsx
+++ app/components/RecentSellTicketPart.tsx
@@ -3,7 +3,7 @@ import SellTicketCard from './SellTicketCard'
 
 export default function RecentSellTicketPart() {
   return (
-    <div className="space-y-2 max-h-[500px] lg:h-[350px] xl:h-[450px] overflow-y-auto">
+    <div className="space-y-2 max-h-[500px] lg:h-[350px] xl:h-[450px] overflow-y-auto scrollbar-custom">
           <SellTicketCard></SellTicketCard>
           <SellTicketCard></SellTicketCard>
           <SellTicketCard></SellTicketCard>
