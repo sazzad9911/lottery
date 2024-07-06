54156f622266e2157a986082eb058ab6063ab55f
Mahadi Hasan <149463189+MahadiHasan1996@users.noreply.github.com>
2024-07-06 12:33:29 +0600

    ok

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,15 +1,24 @@
+'use client'
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Introduction: React.FC = () => {
+  const language = useSelector((state: RootState) => state.language.language);
   return (
     <div className="container mx-auto px-2 md:px-8">
-      <div className=" bg-white text-black mt-6">
-        <h1 className="text-3xl font-normal mb-4">Introduction</h1>
+      <div className=" text-black mt-6">
+        <h1 className="text-3xl font-normal mb-4">
+          {
+            language ==='en'? 'Introduction':'ভূমিকা'
+          }
+        </h1>
         <div className="space-y-4">
+      
           <p className="">
-            The lottery process starts with players purchasing tickets from
-            authorized retailers or online platforms. Each ticket has a unique
-            set of numbers, either chosen by the player or randomly generated.
+          {
+            language ==='en'? 'The lottery process starts with players purchasing tickets from authorized retailers or online platforms. Each ticket has a unique set of numbers, either chosen by the player or randomly generated.':'খেলোয়াড়রা অনুমোদিত খুচরা বিক্রেতা বা অনলাইন প্ল্যাটফর্ম থেকে টিকিট কেনার মাধ্যমে লটারি প্রক্রিয়া শুরু হয়। প্রতিটি টিকিটের সংখ্যার একটি অনন্য সেট রয়েছে, হয় প্লেয়ার দ্বারা বাছাই করা বা এলোমেলোভাবে তৈরি করা।'
+          }
           </p>
           <p className="">
             On the designated draw date, a public drawing occurs where a series
