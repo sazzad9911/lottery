70ef0e25bf139e4fd12002ebd3ab453b853d3736
Mahadi Hasan <149463189+MahadiHasan1996@users.noreply.github.com>
2024-07-06 13:14:24 +0600

    bangla

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,11 +1,14 @@
 "use client";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 import React, { useEffect, useState } from "react";
 import people1 from "../../public/images/People1.png";
 import people2 from "../../public/images/People2.png";
 import bg from "../../public/images/bg.png";
 
 export default function Banner() {
+  const language = useSelector((state: RootState) => state.language.language);
   const images = [people1, people2, people1, people2, people1];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -46,36 +49,35 @@ export default function Banner() {
           ))}
           <div className="absolute right-0 top-20 xl:top-28 max-w-[650px] xl:max-w-[800px]">
             <div className="flex flex-col items-center space-y-2 xl:space-y-4 text-white">
-              <h1 className="font-semibold text-[20px] lg:text-[36px] font-serif">DEAR LOTTERY</h1>
+              <h1 className="font-semibold text-[20px] lg:text-[36px] font-serif">
+                {language === "en" ? "DEAR LOTTERY" : "প্রিয় লটারি"}
+              </h1>
               <div>
-              <p className="font-semibold text-[18px] md:text-[28px] text-center">৳ 6</p>
-              <p className="font-semibold text-[18px] md:text-[28px] text-center font-sans">Ticket</p>
+                <p className="font-semibold text-[18px] md:text-[28px] text-center">
+                  {language === "en" ? "৳ 6" : "৳ ৬"}
+                </p>
+                <p className="font-semibold text-[18px] md:text-[28px] text-center font-sans">
+                  {language === "en" ? "Ticket" : "টিকিট"}
+                </p>
               </div>
-              <p  className="font-normal text-[14px] md:text-[16px]">
-                {"Dear Lottery"} is a compelling drama that revolves around the
-                lives of individuals whose fates are intertwined by a massive
-                lottery win. The story delves into the complexities of luck,
-                greed, and human nature, exploring how sudden wealth can both
-                transform and complicate relationships. As characters navigate
-                their newfound fortunes, they face moral dilemmas, unexpected
-                challenges, and the poignant realization that money cant buy
-                happiness.
+              <p className="font-normal text-[14px] md:text-[16px]">
+                {language === "en"
+                  ? "Dear Lottery is a compelling drama that revolves around the lives of individuals whose fates are intertwined by a massive lottery win. The story delves into the complexities of luck, greed, and human nature, exploring how sudden wealth can both transform and complicate relationships. As characters navigate their newfound fortunes, they face moral dilemmas, unexpected challenges, and the poignant realization that money cant buy happiness."
+                  : "প্রিয় লটারি হল একটি আকর্ষক নাটক যা সেই ব্যক্তিদের জীবনের চারপাশে আবর্তিত হয় যাদের ভাগ্য একটি বিশাল লটারি জয়ের সাথে জড়িত। গল্পটি ভাগ্য, লোভ এবং মানব প্রকৃতির জটিলতাগুলিকে আবিষ্কার করে, কীভাবে আকস্মিক সম্পদ সম্পর্ককে রূপান্তরিত এবং জটিল করে তুলতে পারে। অক্ষরগুলি যখন তাদের নতুন ভাগ্যের দিকে নেভিগেট করে, তারা নৈতিক দ্বিধা, অপ্রত্যাশিত চ্যালেঞ্জ এবং মর্মান্তিক উপলব্ধির মুখোমুখি হয় যে অর্থ সুখ কিনতে পারে না"}
               </p>
-              <p  className="font-normal text-[14px] md:text-[16px] text-[#F7FF01]">
-                Earn 5% commission on every ticket you resell! Join our program
-                today and start making extra money by helping others find
-                tickets to their favourite events!!!
+              <p className="font-normal text-[14px] md:text-[16px] text-[#F7FF01]">
+              {language === "en"
+                  ? "Earn 5% commission on every ticket you resell! Join our program today and start making extra money by helping others find tickets to their favourite events!!!"
+                  : "আপনি পুনরায় বিক্রি করা প্রতিটি টিকিটে 5% কমিশন উপার্জন করুন! আজই আমাদের প্রোগ্রামে যোগ দিন এবং অন্যদের তাদের প্রিয় ইভেন্টের টিকিট খুঁজে পেতে সাহায্য করে অতিরিক্ত অর্থ উপার্জন শুরু করুন!!!"}
               </p>
               <div className="flex gap-4 text-black">
                 <div className="flex justify-center items-center h-10 lg:h-14 w-40 bg-[#F9DC00] rounded-md cursor-pointer hover:shadow-md hover:scale-110 duration-300">
-                  Take a Ticket
+                {language === "en" ? "Take a Ticket" : "টিকিট নিন"} 
                 </div>
                 <div className="flex justify-center items-center h-10 lg:h-14 w-40 bg-[#F9DC00] rounded-md cursor-pointer hover:shadow-md hover:scale-110 duration-300">
-                Join as Reseller
+                {language === "en" ? "Join as Reseller" : "পুনরায় বিক্রেতা হিসাবে যোগদান করুন"}  
                 </div>
-             
               </div>
-
             </div>
           </div>
           <div className="flex justify-center w-full absolute bottom-4">
--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
@@ -7,47 +7,35 @@ const Introduction: React.FC = () => {
   const language = useSelector((state: RootState) => state.language.language);
   return (
     <div className="container mx-auto px-2 md:px-8">
-      <div className=" text-black mt-6">
+      <div className="text-black mt-6">
         <h1 className="text-3xl font-normal mb-4">
-          {
-            language ==='en'? 'Introduction':'ভূমিকা'
-          }
+          {language === "en" ? "Introduction" : "ভূমিকা"}
         </h1>
         <div className="space-y-4">
-      
           <p className="">
-          {
-            language ==='en'? 'The lottery process starts with players purchasing tickets from authorized retailers or online platforms. Each ticket has a unique set of numbers, either chosen by the player or randomly generated.':'খেলোয়াড়রা অনুমোদিত খুচরা বিক্রেতা বা অনলাইন প্ল্যাটফর্ম থেকে টিকিট কেনার মাধ্যমে লটারি প্রক্রিয়া শুরু হয়। প্রতিটি টিকিটের সংখ্যার একটি অনন্য সেট রয়েছে, হয় প্লেয়ার দ্বারা বাছাই করা বা এলোমেলোভাবে তৈরি করা।'
-          }
+            {language === "en"
+              ? "The lottery process starts with players purchasing tickets from authorized retailers or online platforms. Each ticket has a unique set of numbers, either chosen by the player or randomly generated."
+              : "খেলোয়াড়রা অনুমোদিত খুচরা বিক্রেতা বা অনলাইন প্ল্যাটফর্ম থেকে টিকিট কেনার মাধ্যমে লটারি প্রক্রিয়া শুরু হয়। প্রতিটি টিকিটের সংখ্যার একটি অনন্য সেট রয়েছে, হয় প্লেয়ার দ্বারা বাছাই করা বা এলোমেলোভাবে তৈরি করা।"}
           </p>
           <p className="">
-            On the designated draw date, a public drawing occurs where a series
-            of winning numbers are randomly selected. This is usually done with
-            machines that mix and draw numbered balls or through computerized
-            random number generators, ensuring the process is fair and
-            transparent.
+            {language === "en"
+              ? " On the designated draw date, a public drawing occurs where a series of winning numbers are randomly selected. This is usually done with machines that mix and draw numbered balls or through computerized random number generators, ensuring the process is fair and transparent."
+              : "নির্ধারিত ড্র তারিখে, একটি সর্বজনীন অঙ্কন ঘটে যেখানে বিজয়ী সংখ্যার একটি সিরিজ এলোমেলোভাবে নির্বাচন করা হয়। এটি সাধারণত এমন মেশিনের মাধ্যমে করা হয় যা সংখ্যাযুক্ত বলগুলিকে মিশ্রিত করে এবং আঁকে বা কম্পিউটারাইজড এলোমেলো নম্বর জেনারেটরের মাধ্যমে, প্রক্রিয়াটি ন্যায্য এবং স্বচ্ছ তা নিশ্চিত করে।"}
           </p>
           <p className="">
-            After the drawing, players check their tickets against the winning
-            numbers. This can be done at retail locations, online, or via
-            official lottery apps. Matching tickets are identified as winners,
-            with the prize amount depending on how many numbers match and the
-            lottery’s specific rules.
+            {language === "en"
+              ? "After the drawing, players check their tickets against the winning numbers. This can be done at retail locations, online, or via official lottery apps. Matching tickets are identified as winners, with the prize amount depending on how many numbers match and the lottery’s specific rules."
+              : "অঙ্কনের পরে, খেলোয়াড়রা বিজয়ী সংখ্যার বিপরীতে তাদের টিকিট পরীক্ষা করে। এটি খুচরা অবস্থানে, অনলাইনে বা অফিসিয়াল লটারি অ্যাপের মাধ্যমে করা যেতে পারে। ম্যাচিং টিকিটকে বিজয়ী হিসেবে চিহ্নিত করা হয়, পুরস্কারের পরিমাণ কত নম্বরের সাথে মিলছে এবং লটারির নির্দিষ্ট নিয়মের উপর নির্ভর করে।"}
           </p>
           <p className="">
-            Winners must then claim their prizes within a set timeframe,
-            presenting their winning tickets for validation. Small prizes can
-            often be claimed at retail locations, while larger prizes may
-            require a visit to a lottery office or submission through official
-            channels. Payouts are typically made as lump sums or annuities, and
-            taxes may be deducted.
+            {language === "en"
+              ? "Winners must then claim their prizes within a set timeframe, presenting their winning tickets for validation. Small prizes can often be claimed at retail locations, while larger prizes may require a visit to a lottery office or submission through official channels. Payouts are typically made as lump sums or annuities, and taxes may be deducted."
+              : "বিজয়ীদের অবশ্যই একটি নির্দিষ্ট সময়সীমার মধ্যে তাদের পুরস্কার দাবি করতে হবে, বৈধতার জন্য তাদের বিজয়ী টিকিট উপস্থাপন করতে হবে। ছোট পুরষ্কারগুলি প্রায়শই খুচরা অবস্থানগুলিতে দাবি করা যেতে পারে, যখন বড় পুরস্কারগুলির জন্য লটারি অফিসে যাওয়া বা অফিসিয়াল চ্যানেলগুলির মাধ্যমে জমা দেওয়ার প্রয়োজন হতে পারে৷ পেআউটগুলি সাধারণত একক বার্ষিক হিসাবে তৈরি করা হয় এবং কর কাটা যেতে পারে।"}
           </p>
           <p className="">
-            Unclaimed prizes, if not collected within the specified period, are
-            usually redirected to future lottery draws, special promotions, or
-            community programs. Lotteries are closely regulated to maintain
-            integrity, with proceeds often funding public services such as
-            education, infrastructure, and charitable initiatives.
+          {language === "en"
+              ? "Unclaimed prizes, if not collected within the specified period, are usually redirected to future lottery draws, special promotions, or community programs. Lotteries are closely regulated to maintain integrity, with proceeds often funding public services such as education, infrastructure, and charitable initiatives."
+              : "দাবি না করা পুরস্কার, যদি নির্দিষ্ট সময়ের মধ্যে সংগ্রহ না করা হয়, তাহলে সাধারণত ভবিষ্যতে লটারি ড্র, বিশেষ প্রচার বা কমিউনিটি প্রোগ্রামে পুনঃনির্দেশিত হয়। লটারিগুলি অখণ্ডতা বজায় রাখার জন্য ঘনিষ্ঠভাবে নিয়ন্ত্রিত হয়, আয় প্রায়শই শিক্ষা, অবকাঠামো এবং দাতব্য উদ্যোগের মতো সরকারি পরিষেবাগুলিতে অর্থায়ন করে।"}
           </p>
         </div>
       </div>
