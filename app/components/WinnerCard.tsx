0af52533ccd6eeb4d2f77c96e80bf4a18c8b299c
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-07-10 14:39:56 +0600

    winner page done

--- app/components/WinnerCard.tsx
+++ app/components/WinnerCard.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import img from "../../public/images/dummy.png";
+import Image from "next/image";
 
 export default function WinnerCard() {
   return (
-    <div className='bg-red-500'>
-      winner card
+    <div className="flex flex-col items-center">
+      <div className=" h-20 w-20 md:h-40 md:w-40">
+        <Image src={img} alt="dp" className="relative rounded-full"></Image>
+      </div>
+      <div className="text-center space-y-1">
+        <p className="text-[16px] md:text-[18px] font-bold ">
+          Kazi Nazrul Islam Lottery
+        </p>
+        <p className="text-[14px] md:text-[16px] font-light ">
+          1st Prize (10 Lakh)
+        </p>
+        <p className="text-[16px] md:text-[18px] font-normal ">Munia Islam</p>
+        <p className="text-[12px] md:text-[14px] font-light ">
+          Kurigram, Rangpur
+        </p>
+        <p className="text-[12px] md:text-[14px] font-light ">
+          Draw Date: 11 Jun 2021
+        </p>
+      </div>
     </div>
-  )
+  );
 }
--- app/pages/winners/page.tsx
+++ app/pages/winners/page.tsx
@@ -5,7 +5,7 @@ export default function Winners() {
   return (
     <div className="container mx-auto px-2 md:px-8">
       <h1 className='text-[16px] md:text-[20px] font-bold text-center'>Winners</h1>
-      <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 md:gap-4 md:py-10 py-4'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 md:gap-4 lg:gap-6 md:py-10 py-4'>
         <WinnerCard></WinnerCard>
         <WinnerCard></WinnerCard>
         <WinnerCard></WinnerCard>
@@ -17,9 +17,7 @@ export default function Winners() {
         <WinnerCard></WinnerCard>
         <WinnerCard></WinnerCard>
         <WinnerCard></WinnerCard>
-        <WinnerCard></WinnerCard>
-        <WinnerCard></WinnerCard>
-        <WinnerCard></WinnerCard>
+    
         <WinnerCard></WinnerCard>
       </div>
     </div>
