7f0ae6380215a80a7ebe285bfd7cd3cd61fd34eb
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-07-10 13:10:19 +0600

    added winner page

---
+++ app/components/WinnerCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WinnerCard() {
+  return (
+    <div className='bg-red-500'>
+      winner card
+    </div>
+  )
+}
--- app/pages/winners/page.tsx
+++ app/pages/winners/page.tsx
@@ -1,10 +1,27 @@
+import WinnerCard from '@/app/components/WinnerCard'
 import React from 'react'
 
 export default function Winners() {
   return (
-    <div>
-      Winners
+    <div className="container mx-auto px-2 md:px-8">
+      <h1 className='text-[16px] md:text-[20px] font-bold text-center'>Winners</h1>
+      <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 md:gap-4 md:py-10 py-4'>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+        <WinnerCard></WinnerCard>
+      </div>
     </div>
   )
 }
-
