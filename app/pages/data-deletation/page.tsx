04f4468bdaa31939345956b94311be0ae5d0c888
Mahadi Hasan <149463189+MahadiHasan1996@users.noreply.github.com>
2024-07-29 10:25:44 +0600

    ok

--- app/pages/data-deletation/page.tsx
+++ app/pages/data-deletation/page.tsx
@@ -27,25 +27,25 @@ const DataDeletionPolicy = () => {
             ? "1. Requesting Data Deletion:"
             : "১. ডেটা মুছে ফেলার অনুরোধ করা:"}
         </h3>
-        <ul className="list-disc list-inside ml-4 mb-4">
-          <p>
+        {/* <ul className="list-disc list-inside ml-4 mb-4"> */}
+          <p className="mb-4">
           {language === "en"
             ? "You have the right to request the deletion of your personal data from our systems at any time.This includes any information you have provided to us during the purchase of lottery tickets, registration,or other interactions with Dear Lottery."
             : "আমাদের সিস্টেম থেকে যেকোনো সময় আপনার ব্যক্তিগত ডেটা মুছে ফেলার অনুরোধ করার অধিকার আপনার আছে। এতে আপনার কাছে থাকা যেকোনো তথ্য অন্তর্ভুক্ত রয়েছে। ডিয়ার লটারির সাথে লটারির টিকিট, রেজিস্ট্রেশন বা অন্যান্য মিথস্ক্রিয়া কেনার সময় আমাদের সরবরাহ করা হয়েছে।"}           
           </p>
-        </ul>
+        {/* </ul> */}
         <h3 className="text-xl font-medium mb-1">
         {language === "en"
             ? "2. How to Request Data Deletion:"
             : "২. কীভাবে ডেটা মুছে ফেলার অনুরোধ করবেন:"}          
         </h3>
-        <ul className="list-disc list-inside ml-4">
-          <p>
+        {/* <ul className="list-disc list-inside ml-4"> */}
+          <p className="mb-4">
           {language === "en"
             ? "To request the deletion of your data, please contact our customer support team at [Your Email Address] or [Your Phone Number]. Provide your name, email address, and any other relevant information that will help us locate your data."
             : "আপনার ডেটা মুছে ফেলার অনুরোধ করতে, অনুগ্রহ করে [আপনার ইমেল ঠিকানা] বা [আপনার ফোন নম্বর] এ আমাদের গ্রাহক সহায়তা দলের সাথে যোগাযোগ করুন। আপনার নাম, ইমেল ঠিকানা, এবং অন্য কোন প্রাসঙ্গিক তথ্য প্রদান করুন যা আমাদের আপনার ডেটা সনাক্ত করতে সাহায্য করবে।"}           
           </p>
-        </ul>
+        {/* </ul> */}
       </section>
 
       <section className="mb-6">
@@ -60,19 +60,25 @@ const DataDeletionPolicy = () => {
             : "১. যাচাইকরণ:"}          
           </h3>
         <p className="mb-4">
-          Upon receiving your data deletion request, we will verify your
-          identity to ensure the security and integrity of the process. This may
-          involve confirming your identity through email, phone, or other means.
+        {language === "en"
+            ? "Upon receiving your data deletion request, we will verify your identity to ensure the security and integrity of the process. This may involve confirming your identity through email, phone, or other means."
+            : "আপনার ডেটা মুছে ফেলার অনুরোধ পাওয়ার পর, আমরা প্রক্রিয়াটির নিরাপত্তা এবং অখণ্ডতা নিশ্চিত করতে আপনার পরিচয় যাচাই করব। এটি ইমেল, ফোন বা অন্যান্য উপায়ে আপনার পরিচয় নিশ্চিত করতে জড়িত হতে পারে।"}        
         </p>
         <h3 className="text-xl font-medium mb-1">
-          2. Review and Confirmation:
+        {language === "en"
+            ? "2. Review and Confirmation:"
+            : "২. পর্যালোচনা এবং নিশ্চিতকরণ:"}         
         </h3>
         <p className="mb-4">
-          We will review your request and confirm the types of data you wish to
-          be deleted. We will also inform you of any data that we are required
-          to retain by law, which cannot be deleted.
+        {language === "en"
+            ? "We will review your request and confirm the types of data you wish to be deleted. We will also inform you of any data that we are required to retain by law, which cannot be deleted."
+            : "আমরা আপনার অনুরোধ পর্যালোচনা করব এবং আপনি যে ধরনের ডেটা মুছে ফেলতে চান তা নিশ্চিত করব। এছাড়াও আমরা আপনাকে এমন কোনো তথ্য জানাব যা আমাদের আইন দ্বারা ধরে রাখতে হবে, যা মুছে ফেলা যাবে না।"}     
         </p>
-        <h3 className="text-xl font-medium mb-1">3. Data Deletion:</h3>
+        <h3 className="text-xl font-medium mb-1">
+        {language === "en"
+            ? "3. Data Deletion:"
+            : "৩. ডেটা মুছে ফেলা:"}         
+          </h3>
         <p className="mb-4">
           Once your request is verified and approved, we will delete your
           personal data from our active systems and databases. This process will
