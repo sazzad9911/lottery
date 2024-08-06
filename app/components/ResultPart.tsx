2571068cbc581ab221cffef3c76d43b89254703a
Mahadi Hasan <149463189+MahadiHasan1996@users.noreply.github.com>
2024-08-06 15:50:26 +0600

    Resultpart

--- app/components/ResultPart.tsx
+++ app/components/ResultPart.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Resultcard from './ResultCard'
+import React from "react";
+import Resultcard from "./ResultCard";
 
 function ResultPart() {
   return (
     <div className="space-y-2 max-h-[500px] lg:h-[350px] xl:h-[450px] overflow-y-auto scrollbar-custom">
-      
       <Resultcard></Resultcard>
       <Resultcard></Resultcard>
       <Resultcard></Resultcard>
       <Resultcard></Resultcard>
       <Resultcard></Resultcard>
-
     </div>
-  )
+  );
 }
 
-export default ResultPart
+export default ResultPart;
