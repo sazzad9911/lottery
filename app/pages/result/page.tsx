1f368dac1d853525598d4ac023692ac43d93f35c
Mahadi Hasan <149463189+MahadiHasan1996@users.noreply.github.com>
2024-08-10 12:08:27 +0600

    result page translate

--- app/pages/result/page.tsx
+++ app/pages/result/page.tsx
@@ -4,14 +4,18 @@ import { NextPage } from "next";
 import ResultPart from "@/app/components/ResultPart";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Paginations from "@/app/components/Pagination";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/redux/store";
 
 const theme = createTheme();
 
 const PaginationPage: NextPage = () => {
+  const language = useSelector((state: RootState) => state.language.language);
   return (
     <div>
       <p className="text-center text-[20px] sm:text-[26px] lg:text-[32px] text-[#000000 mb-6">
-        Published Lottery Results
+      {language === "en" ? "Published Lottery Results" : "প্রকাশিত লটারি ফলাফল"}
+        
       </p>
       <div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 px-2 sm:px-5 ">
