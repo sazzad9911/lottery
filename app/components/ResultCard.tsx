f5c504f89337a414bfb7c189a2a3b5d867eb90c9
Mahadi Hasan <149463189+MahadiHasan1996@users.noreply.github.com>
2024-08-12 13:07:31 +0600

    Draw date

--- app/components/ResultCard.tsx
+++ app/components/ResultCard.tsx
@@ -9,12 +9,12 @@ function ResultCard() {
     <div className="bg-[#D94F72] rounded-md px-4 py-2">
     <div className="flex justify-between items-center">
       <p className="text-[16px] md:text-[20px] font-bold text-white">
-      {language === "en" ? "Kazi Nazrul Islam Lottery" : "Kazi Nazrul Islam Lottery"}
+      {language === "en" ? "Kazi Nazrul Islam Lottery" : "কাজী নজরুল ইসলাম লটারি"}
       </p>
       
     </div>
     <p className="text-[14px] md:text-[16px] font-light text-white mt-1">
-    {language === "en" ? "Draw Date: 12 June 2024" : "Draw Date: 12 June 2024"}
+    {language === "en" ? "Draw Date: 12 june 2024" : "ড্র তারিখ: ১২ জুন ২০২৪"}
     </p>
   </div>
   )
